ff1bc4e2e2197cdacdb671a526823f67236f5b19
LucianoGauna <luciano.927@hotmail.com>
2023-02-12 18:27:35 -0300

    ...

--- src/app.js
+++ src/app.js
@@ -1,46 +1,23 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
-const path = require ('path');
-const PORT = 3003
+const path = require('path');
+const PORT = 3003;
 
 const mainRoutes = require('./routes/main');
-const productRoutes = require ('./routes/products')
-const userRoutes = require('./routes/user')
+const productRoutes = require('./routes/products');
+const userRoutes = require('./routes/user');
 
 // view engine setup
 app.set('view engine', 'ejs');
-app.set('views', path.resolve('./src/views'))
+app.set('views', path.resolve('./src/views'));
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use(productRoutes)
-app.use(userRoutes)
-app.use(mainRoutes)
-
+app.use(productRoutes);
+app.use(userRoutes);
+app.use(mainRoutes);
 
 app.listen(PORT, () => {
-    console.log(`server on ${PORT}`);
-})
-// app.get('/', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, './views/home.html'))
-// });
+  console.log(`server on ${PORT}`);
+});
 
-// app.get('/productDetail', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
-// });
-
-// app.get('/login', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './views/login.html'))
-// });
-
-// app.get('/register', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './views/register.html'))
-// });
-
-// app.get('/productCart', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './views/cart.html'))
-// });
-
-// app.get('/terms-conditions', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './views/terms-conditions.html'))
-// });
