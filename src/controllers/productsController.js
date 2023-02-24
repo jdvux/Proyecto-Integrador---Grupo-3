df78ecf3a26d1d06d42b2df51f4fc4009a4d4d74
Juan Vergara <abgjvergara@gmail.com>
2023-02-24 12:59:15 -0300

    fix edit

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -63,7 +63,7 @@ const productController = {
       product.category = req.body.category;
       product.oldPrice = req.body.oldPrice;
       product.price = req.body.price;
-      if((req.files).length !==0) {
+      if(req.files) {
         req.files.forEach(file=>{images.push("/products/" + file.filename)})
         product.image = images 
       };
