e6617bbe8d1483e718ad3cdbbb289c4fea7e048b
Juan Vergara <abgjvergara@gmail.com>
2023-03-06 18:05:32 -0300

    fixes

--- src/middlewares/registerValidations.js
+++ src/middlewares/registerValidations.js
@@ -1,4 +1,8 @@
 const { body } = require('express-validator');
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const registerValidations = [
     body("nameRegister")
@@ -11,7 +15,14 @@ const registerValidations = [
 
     body("emailRegister")
         .notEmpty().withMessage("Debes escribir tu correo electrónico").bail()
-        .isEmail().withMessage("El del correo formato debe ser correo@correo.xxx"),
+        .isEmail().withMessage("El del correo formato debe ser correo@correo.xxx")
+        .custom((value, { req }) => {
+            let found = users.find(user => user.email == req.body.emailRegister);
+            if (found && found !== undefined) {
+                throw new Error("Ese correo electrónico ya se encuentra en uso");
+            }
+            return true;
+        }),
 
     body("passwordRegister")
         .isLength({ min: 8 }).withMessage("La contraseña debe contener al menos 8 caracteres").bail()
