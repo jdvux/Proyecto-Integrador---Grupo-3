5d0271cd5f6256ccd5c1af2648b9cc6b75276eb0
Juan Vergara <abgjvergara@gmail.com>
2023-05-20 01:41:56 -0400

    apis

--- src/controllers/api/productsController.js
+++ src/controllers/api/productsController.js
@@ -1,17 +1,19 @@
+const { Association } = require('sequelize');
 const {Product, Image, ProductBrands, ProductCategories} = require('../../database/models');
 
 const productsController = {
   list: (req, res) => {  
     Product.findAll(
       {
-        include: ['brands', 'categories'],
+        include: ['categories'],
         attributes: ['id', 'name', 'description']
       }
     ).then(products => {
         return res.json({ 
             info: {
               count: products.length,
-              countByCategory: "",
+              countByCategory: {
+              },
             },
             results: products 
           });
@@ -21,10 +23,23 @@ const productsController = {
   detail: (req, res) => {
     Product.findByPk(req.params.id, 
       {
-        include: ['images']
+        include: [{
+          association: 'images',
+          attributes: ["name"]
+        }]
       })
       .then(product => {
-        return res.json({ product });
+        return res.json({ 
+          id: product.id,
+          name: product.name,
+          description: product.description,
+          size: product.size,
+          price: product.price,
+          price_with_no_discount: product.price_with_no_discount,
+          category_id: product.category_id,
+          brand_id: product.brand_id,
+          image: "http://localhost:3003/images/products/" + product.images[0].name
+        });
       })
   }
 };
